595dfb5a3560fe83ba144cfabd7b0597e8833134
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-16 21:43:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function promiseFactory() {
 	return new Promise((resolve, reject) => {
-		setTimeOut(() => {
+		setTimeout(() => {
 			resolve('Hello, world!');
 		}, 1000)
 	})

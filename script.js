1cb7e44f7fe413d805b99fc4a8e6ddc69c53cd60
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-16 21:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function promiseFactory() {
+	return new Promise((resolve, reject) => {
+		setTimeOut(() => {
+			resolve('Hello, world!');
+		}, 1000)
+	})
+}
+
+promiseFactory()
+.then(data => document.getElementById('output').textContent = data);
